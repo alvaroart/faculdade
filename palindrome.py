a4807696457dd2712c3b9fa032b6daa909d2f645
Alvaro <alvaro.rosa1985@gmail.com>
2016-08-22 22:18:26 -0300

    ad1-exercicio2 TEST de inversão.

--- palindrome.py
+++ palindrome.py
@@ -1,18 +1,2 @@
-palavra = input('Palavra: ')
-
-palavra = palavra.casefold()
-
-rev = palavra.casefold()[::-1]
-
-palavra = palavra.replace(' ', '')
-rev = rev.replace(' ', '')
-
-#print ("\n\n", rev, "\n\n")
-
-#print ("\n\n", rev, "\n\n")
-#print (palavra. (' ', '')())
-
-if palavra == rev:
-    print('%s é Palíndrome' %palavra)
-else:
-    print('%s não é Palíndrome' %rev)
+palavra = 'atecubanos'
+print (palavra[::-1])
