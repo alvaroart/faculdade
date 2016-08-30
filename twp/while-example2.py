7052ef56a9f9664724880f42f845721a52f21a74
Alvaro <alvaro.rosa1985@gmail.com>
2016-08-30 16:32:50 -0300

    Imprimir numeros pares entre 0 e um numero fornecido usando if.

--- twp/while-example2.py
+++ twp/while-example2.py
@@ -1,6 +1,8 @@
+#Imprimir os numeros pares entre 0 e um numero foenrcido usando if
+
 fim = int(input ("Digite o numero final: "))
 x = 1
 while x <= fim:
-	if x%2 = 0:
+	if x%2 == 0:
 		print (x)
 	x = x + 1
