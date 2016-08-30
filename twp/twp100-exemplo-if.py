31442b2f3f4f97828cf2208d5e22c01ea0e53e68
Alvaro <alvaro.rosa1985@gmail.com>
2016-08-30 16:18:17 -0300

    palindrome-test

---
+++ twp/twp100-exemplo-if.py
@@ -0,0 +1,6 @@
+#exemplo curso twp-100 condiçoes
+idade = int(input("\n\tDigite a idade do seu carro:"))
+if idade <=3:
+    print("\nSeu carro é novo")
+if idade > 3 :
+    print("\nSeu carro é velho!")
