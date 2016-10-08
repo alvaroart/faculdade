8688a8e9052691b310dfd851bf0e1b1f1e68b544
alvaro <alvarorosa@id.uff.br>
2016-10-08 14:39:02 -0300

    aula08 _exemplo3

---
+++ aula8-listas003.py
@@ -0,0 +1,8 @@
+mercado = [["pera", 100, 4.9],["manga", 20, 3.9],["uva", 30, 5.9], ["caju"], 15, 3.5, ["banana"], 12, 1.99]
+print(mercado)
+
+#mercado[1][2] *=0.5
+#print(mercado)
+
+mercado[3][1] -=10
+print(mercado)
