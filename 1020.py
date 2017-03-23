9455aa47f7930a977f170697c2a4fe9dc480bd1a
Alvaro Teixeira <alvaro.rosa1985@gmail.com>
2017-03-23 20:30:19 -0300

    Atualizado conforme uri judge online.21-03-17

--- 1020.py
+++ 1020.py
@@ -1,5 +1,6 @@
 x=int(input())
 ano=x/365
-mes=(x%365)/30
-dia=(x%365)-30
-print("ANO(s)%d \nMES(ES)%d\nDia(s)%d" %(ano,mes,dia))
+aux=x%365
+mes=aux/30
+dia=aux%30
+print("%d ano(s)""\n%d mes(es)""\n%d dia(s)" %(ano,mes,dia))
