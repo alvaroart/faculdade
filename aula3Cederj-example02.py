60b46c3ea058798bc33e134dbd4c9228a9061546
alvaroart <alvaro.rosa1985@gmail.com>
2018-07-15 00:36:56 -0300

    '''aula 3  exemplo 2'''
    Signed-off-by: alvaroart <alvaro.rosa1985@gmail.com>

---
+++ aula3Cederj-example02.py
@@ -0,0 +1,6 @@
+'''aula 3  exemplo 2'''
+salario= float(input("Diga seu salario atual: "))
+if salario>1000:
+    inps=salario * 0.10
+    impostoRenda = salario * 0.15
+    print("\n\tValor do INPS:", inps, "e do I.R.:", impostoRenda)
